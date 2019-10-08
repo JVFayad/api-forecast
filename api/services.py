7b19681a24addb9e2a9624ee678e25cb8b19628a
JVFayad <joao.victor.fa2014@gmail.com>
2019-10-07 21:07:14 -0300

    fix

--- api/services.py
+++ api/services.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 from .config import API_ADV_TOKEN
@@ -9,10 +8,11 @@ def get_api_advisor_data(city_id):
     Make a request to API Advisor and 
     return data and status code
     """
-    url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/{0}/days/15?token={1}'.format(
-        city_id,
-        API_ADV_TOKEN
-    )
+    url = ('http://apiadvisor.climatempo.com.br/api/v1/'
+        'forecast/locale/{0}/days/15?token={1}').format(
+            city_id,
+            API_ADV_TOKEN
+        )
 
     response = requests.get(url)
 
